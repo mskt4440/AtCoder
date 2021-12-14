af975f5a7db9e035e66bee7d39dcdbcc822d262c
Masa Kato <mskt4440@gmail.com>
2021-12-14 16:58:57 +0900

    abc128

--- abc128/b.py
+++ abc128/b.py
@@ -1,20 +1,76 @@
 #
 # abc128 b
 #
+import sys
+from io import StringIO
+import unittest
 
 
-def main():
-    n = int(input())
-    L = []
-    for i in range(n):
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """6
+khabarovsk 20
+moscow 10
+kazan 50
+kazan 35
+moscow 60
+khabarovsk 40"""
+        output = """3
+4
+6
+1
+5
+2"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """10
+yakutsk 10
+yakutsk 20
+yakutsk 30
+yakutsk 40
+yakutsk 50
+yakutsk 60
+yakutsk 70
+yakutsk 80
+yakutsk 90
+yakutsk 100"""
+        output = """10
+9
+8
+7
+6
+5
+4
+3
+2
+1"""
+        self.assertIO(input, output)
+
+
+def resolve():
+    N = int(input())
+    ISP = []
+    for i in range(N):
         s, p = input().split()
-        L.append([i, s, int(p)])
+        p = int(p)
+        isp = [i+1, s, p]
+        ISP.append(isp)
 
-    L = sorted(L, key=lambda x: x[2], reverse=True)
-    L = sorted(L, key=lambda x: x[1])
-    for i in range(n):
-        print(L[i][0]+1)
+    ISP.sort(key=lambda x: (x[1], -x[2]))
+    for i, s, p in ISP:
+        print(i)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    # unittest.main()
+    resolve()
